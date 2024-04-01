33c33291dedc7a44e21e9197dd9ad9b1f27b2b01
Dmutre <104783173+Dmutre@users.noreply.github.com>
2024-04-01 20:35:36 +0200

    Added matrix endpoint

--- python-docker/spaceship/routers/api.py
+++ python-docker/spaceship/routers/api.py
@@ -1,7 +1,20 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
+@router.get('/matrices')
+def generate_and_multiply_matrices():
+    matrix_a = np.random.randint(0, 101, size=(10, 10))
+    matrix_b = np.random.randint(0, 101, size=(10, 10))
+
+    product = np.dot(matrix_a, matrix_b)
+
+    return {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist(),
+    }
 
 @router.get('')
 def hello_world() -> dict:
